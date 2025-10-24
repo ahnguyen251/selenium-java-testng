b456883687ae6c87bcd6734404427d7c5ffc6db4
Ahnguyen251 <Ngocanh25102004@gmail.com>
2025-10-24 08:31:44 +0700

    Add Class 02

--- src/test/java/selenium/Class_01.java
+++ src/test/java/selenium/Class_01.java
@@ -26,6 +26,7 @@ public class Class_01 {
     public void TC_03_Run_On_Edge() {
         driver = new EdgeDriver();
         driver.get("https://api-dev.epicpartner.vn/authenticate/login?ReturnUrl=%2Fconnect%2Fauthorize%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fevents2-dev.epicpartner.vn%252Fauth%252Flogin%252Fconnect-authorize%26client_id%3Dclient-angular%26scope%3Dopenid%2520offline_access%26state%3DHRXUreCUmdiZAh6lwUqQ%26code_challenge%3Dr0mVlnzuokV-tR05OBxSJmDrSbqkHsCXtagdWNl9_bg%26code_challenge_method%3DS256");
-        driver.quit();
+        //driver.quit();
+
     }
 }
